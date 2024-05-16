7a70ccbb3301a98ddc34609491bd85ff8e2736fa
Kittipak <67796659+AlienX77-cmd@users.noreply.github.com>
2024-05-16 12:02:33 +0700

    Add files via upload

--- Machine_Maintenance.py
+++ Machine_Maintenance.py
@@ -27,13 +27,13 @@ with open('./model.pkl', 'rb') as model_file:
 col1, col2 = st.columns(2)
 
 with col1:
-    air = st.number_input(label='Air Temperature')
-    process = st.number_input(label='Process Temperature')
-    rpm = st.number_input(label='Rotational Speed')
+    air = st.number_input(label='Air Temperature [K]')
+    process = st.number_input(label='Process Temperature [K]')
+    rpm = st.number_input(label='Rotational Speed [rpm]')
 
 with col2:
-    torque = st.number_input(label='Torque')
-    tool_wear = st.number_input(label='Tool Wear')
+    torque = st.number_input(label='Torque [Nm]')
+    tool_wear = st.number_input(label='Tool Wear [min]')
     type = st.selectbox(label='Type', options=['Low', 'Medium', 'High'])
 
 # Function to predict the input
